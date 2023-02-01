68d1a36fee68c62bc763663c72ac5222cffaf124
Alilovic Ana <alilovic220799@gmail.com>
2023-02-01 16:39:13 +0100

    add  b64toBlob

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,24 +1,47 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import profile from './img/profile.png'
 
+function b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    var byteCharacters =atob(b64Data);
+    var byteArrays = [];
+  
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+  
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+  
+      var byteArray = new Uint8Array(byteNumbers);
+  
+      byteArrays.push(byteArray);
+    }
+  
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
 function ImageUploader() {
-    const [image, setImage] = useState({});
-    const handleFileUpload = event => {
+    const [image, setImage] = useState(profile);
+    const handleFileChange = async event => {
         console.log(event);
-        setImage(event.target.value.replace("C:\\fakepath\\", ""));
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onloadend = () => {
+            setImage(reader.result);
+        };
     }
 
     const handleSubmit = async e => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append('image', image);
-        // fetch('http://localhost:3001/upload', {
-        //     method:"post",
-        //     body:formData
-        // }).then((res)=>res.text()).then((resBody)=>{
-        //     console.log(resBody);
-        // });
-
+        const base64Image = image;
+        const blob = b64toBlob(base64Image, 'image/jpeg'); // Convert base64 to Blob
+        formData.append('image', blob, 'image.jpeg'); // Append Blob to FormData
+        // formData.append('image', image);
         try {
             const res = await axios.post('http://localhost:3001/upload', formData, {
                 headers: {
@@ -32,11 +55,12 @@ function ImageUploader() {
     };
 
     return (
-        <div className='App'>
-            <input type="file" name="image" onChange={handleFileUpload} />
-            <button onClick={handleSubmit}>Upload</button>
-        </div>
+        <form encType="multipart/form-data" onSubmit={handleSubmit}>
+            <input type="file" name="image" onChange={handleFileChange} />
+            <button type="submit">Submit</button>
+        </form>
     );
+
 }
 
 export default ImageUploader;
